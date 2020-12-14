042c9b4c6adc557ca57321b3e012984e721490da
ganzinn <ganzinnpg@gmail.com>
2020-12-14 17:04:06 +0900

    itemモデル作成

---
+++ app/models/item.rb
@@ -0,0 +1,47 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_payer
+  belongs_to :shipping_from_area
+  belongs_to :shipping_duration
+
+  belongs_to :owner, class_name: 'User'
+
+  has_one_attached :image
+
+  # 以下バリデーション ※「with_options」は使用しない
+
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true,  length: { maximum: 1000 }
+  validates :category_id, numericality: {
+    greater_than_or_equal_to: 1, less_than_or_equal_to: 10,
+    message: 'Select'
+  }
+  validates :condition_id, numericality: {
+    greater_than_or_equal_to: 1, less_than_or_equal_to: 6,
+    message: 'Select'
+  }
+  validates :shipping_payer_id,
+    numericality: {
+      greater_than_or_equal_to: 1, less_than_or_equal_to: 2,
+      message: 'Select'
+    }
+  validates :shipping_from_area_id,
+    numericality: {
+      greater_than_or_equal_to: 1, less_than_or_equal_to: 47,
+      message: 'Select'
+    }
+  validates :shipping_duration_id,
+    numericality: {
+      greater_than_or_equal_to: 1, less_than_or_equal_to: 3,
+      message: 'Select'
+    }
+  validates :price,
+    presence: true,
+    numericality: {
+      greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+      message: 'Out of setting range'
+    }
+end
