1bd3316d15592985010f6bc8f53bc046a31ed6da
ganzinn <ganzinnpg@gmail.com>
2020-12-14 17:05:08 +0900

    テスト作成

---
+++ spec/factories/items.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :item do
+    # owner_id
+    name { 'テスト商品' }
+    description { '説明' }
+    category_id { 1 }
+    condition_id { 1 }
+    shipping_payer_id { 1 }
+    shipping_from_area_id { 1 }
+    shipping_duration_id { 1 }
+    price { 1000 }
+
+    association :owner, factory: :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
