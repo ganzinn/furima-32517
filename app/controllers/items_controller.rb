c70d62607184adee2faa653432b0d3f6b5443c62
ganzinn <ganzinnpg@gmail.com>
2020-12-14 17:03:03 +0900

    コントローラー追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,34 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :description,
+      :category_id,
+      :condition_id,
+      :shipping_payer_id,
+      :shipping_from_area_id,
+      :shipping_duration_id,
+      :price
+    ).merge(owner_id: current_user.id)
   end
 end
