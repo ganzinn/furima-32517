61a17dfdfbe25e0badc787aaa5bee1f8eb26843a
ganzinn <ganzinnpg@gmail.com>
2020-12-15 12:22:00 +0900

    手数料計算機能（JS）の修正

--- app/javascript/packs/items/fee_calc.js
+++ app/javascript/packs/items/fee_calc.js
@@ -2,14 +2,15 @@ function fee_calc(){
   const price = document.getElementById("item-price");
   const add_fee = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
-  
-  price.addEventListener("keyup",(e) => {
+  function fee_calc_logic(){
     const add_fee_value = Math.floor(price.value * 0.1)
     const profit_value = price.value - add_fee_value
-
     add_fee.innerText = add_fee_value.toLocaleString();
     profit.innerHTML = profit_value.toLocaleString();
-
+  }
+  fee_calc_logic();
+  price.addEventListener("keyup",(e) => {
+    fee_calc_logic();
   });
 }
 window.addEventListener("load", fee_calc);
